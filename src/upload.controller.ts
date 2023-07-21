c1776145025768763a49181ac47896117177a795
monkeyking123g <dimaipatii1994@gmail.com>
2023-07-21 21:38:11 +0200

    update resize image

--- src/upload.controller.ts
+++ src/upload.controller.ts
@@ -1,10 +1,11 @@
 import * as path from 'path';
-import sharp from 'sharp';
+import * as sharp from 'sharp';
+import { v4 as uuidv4 } from 'uuid';
 import { Controller, Post, UploadedFile, UseInterceptors, Get, Param, Res } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { multerOptions } from './multer-config';
 import { Response } from 'express';
-
+import Resize from "./resize";
 
 
 async function reduceImageSize(originalPath: string, outputPath: string, width: number, height: number, quality: number, format: 'jpeg' | 'png'): Promise<void> {
@@ -20,6 +21,11 @@ async function reduceImageSize(originalPath: string, outputPath: string, width: 
 
 @Controller('upload')
 export class UploadController {
+  private readonly resize: Resize;
+
+  constructor() {
+    this.resize = new Resize('./uploads');
+  }
 
   @Get(':filename')
   async serveImage(@Param('filename') filename: string, @Res() res: Response) {
@@ -30,21 +36,25 @@ export class UploadController {
   @Post()
   @UseInterceptors(FileInterceptor('image', multerOptions))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    const originalImagePath = file.path; // Get the path of the uploaded image
-    const jpegOutputPath = './uploads/reduced-image.jpeg'; // Destination path for the reduced JPEG image
-    const pngOutputPath = './uploads/reduced-image.png'; // Destination path for the reduced PNG image
+    const originalImagePath = file.path; 
+  
+    const fileExtension = path.extname(originalImagePath);
 
-    // Reduce the image size to 800x600 with 80% quality for JPEG format
-    await reduceImageSize(originalImagePath, jpegOutputPath, 800, 600, 80, 'jpeg');
+    let outputPath: string;
+    if (fileExtension === '.png') {
+      outputPath = `./uploads/${uuidv4()}.png`; 
+    } else if (fileExtension === '.jpeg' || fileExtension === '.jpg') {
+      outputPath = `./uploads/${uuidv4()}.jpeg`; 
+    } else {
+     
+      throw new Error('Unsupported file format');
+    }
 
-    // Reduce the image size to 800x600 with 80% quality for PNG format
-    await reduceImageSize(originalImagePath, pngOutputPath, 800, 600, 80, 'png');
+    await reduceImageSize(originalImagePath, outputPath, 800, 600, 80, fileExtension === '.png' ? 'png' : 'jpeg');
 
-    // Return the response, or you can do further processing here
     return {
       originalFilename: file.filename,
-      jpegFilename: 'reduced-image.jpeg',
-      pngFilename: 'reduced-image.png',
+      resizeFilename: 'reduced-image.png',
     };
   }
 }
