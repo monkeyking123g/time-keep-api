ab84016da808b041c2fa7ad312c2c309cda79ab4
monkeyking123g <dimaipatii1994@gmail.com>
2023-08-12 17:57:40 +0200

    add cros politisy

--- app/src/main.ts
+++ app/src/main.ts
@@ -5,6 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: 'https://refactory-time-money.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  });
   app.useGlobalPipes(new ValidationPipe());
   if (!fs.existsSync('./uploads')) {
     fs.mkdirSync('./uploads');
